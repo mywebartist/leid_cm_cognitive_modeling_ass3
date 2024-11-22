ed4de1f884190f6f041bb080a4ff66b91c9ddef8
infamous-flu <s3590712@vuw.leidenuniv.nl>
2024-11-22 07:02:27 +0100

    bugged but working 4 disks

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class TowerOfHanoi(Model):
     def move(self, disk, dest):
         disk = int(disk)
         if dest not in self.pegs:
-            raise ValueError(f'Invalid destination peg {dest}. Must be one of: A, B, C')
+            return
         src = None
         for key, peg in self.pegs.items():
             if peg and peg[0] == disk:
@@ -56,7 +56,7 @@ class TowerOfHanoi(Model):
         if src_peg[0] != disk:
             self.m = src_peg[src_peg.index(disk)-1]
             return
-        if dst_peg and dst_peg[-1] < disk:
+        if dst_peg and dst_peg[0] < disk:
             self.m = dst_peg[-1]
             return
         self.m = None
@@ -68,61 +68,102 @@ class TowerOfHanoi(Model):
 class WrapperEnv(Model):
 
     print('start...\n')
-    towers = TowerOfHanoi(3)
+    towers = TowerOfHanoi(4)
 
 
-class MyAgent(ACTR):
+class AlgorithmicAgent(ACTR):
 
     goal = Buffer()
     imaginal = Buffer()
     DMBuffer = Buffer()
     DM = Memory(DMBuffer)
 
+    debug = False
+
     def init():
-        goal.set('p:3 pTo:C d:None dTo:None check:False recall:False count:False')
+        goal.set('p:4 pTo:C d:None dTo:None check:False recall:False tree:None satisfied:False')
+        imaginal.set('None')
+        DM.add('count 0 1')
         DM.add('count 1 2')
         DM.add('count 2 3')
+        DM.add('count 3 4')
+        DM.add('count 5 6')
+        if debug:
+            print(f'\nINIT: {goal.chunk}')
 
-    def check(goal='d:!None?d dTo:!None?dTo check:False?check'):
-        towers.check(d, dTo)
-        goal.modify(check='True')
-
-    def start_tower(goal='p:!1?p pTo:?pTo d:None?d dTo:None?dTo check:False recall:False'):
+    def start_tower(goal='p:!1?p pTo:?pTo d:None?d dTo:None?dTo check:False recall:False', imaginal='None'):
+        imaginal.set('0')
         goal.modify(d=p, dTo=pTo)
+        if debug:
+            print(f'START: {goal.chunk}')
 
-    def final_move(goal='p:1?p pTo:?pTo d:None?d dTo:None?dTo check:False recall:False'):
+    def final_move(goal='p:1?p pTo:?pTo d:None dTo:None check:False recall:False', imaginal='None'):
         towers.move(p, pTo)
         goal.clear()
+        if debug:
+            print(f'\nFINAL: {goal.chunk}')
         self.stop()
 
-    def subgoal_blocker(goal='p:?p d:?d dTo:?dTo check:True?check', towers='m:!None?m y:?y'):
-        DM.add('?p ?m ?y ?d ?dTo')
-        goal.modify(d=m, dTo=y, check='False')
+    def check(goal='d:!None?d dTo:!None?dTo check:False?check tree:None', imaginal='?depth'):
+        towers.check(d, dTo)
+        goal.modify(check='True')
+        if debug:
+            print(f'CHECK: {goal.chunk}    DEPTH: {depth}')
 
-    def move(goal='p:?p pTo:?pTo d:!None?d dTo:!None?dTo check:True?check recall:?recall', towers='m:None?m y:!None?y'):
+    def subgoal(goal='check:True tree:None?tree', towers='m:!None?m y:?y', imaginal='?depth'):
+        DM.request('count ?depth ?')
+        goal.modify(tree='Down')
+        if debug:
+            print(f'SUBGOAL: {goal.chunk}    DEPTH: {depth}')
+
+    def down(goal='tree:Down?tree', DMBuffer='count ? ?n', imaginal='?depth'):
+        imaginal.set('?n')
+        goal.modify(tree='Blocked')
+        if debug:
+            print(f'DOWN: {goal.chunk}    DEPTH: {depth}')
+
+    def blocker(goal='p:?p d:?d dTo:?dTo check:True?check tree:Blocked?tree', towers='m:!None?m y:?y', imaginal='?depth'):
+        DM.add('?p ?depth ?m ?y ?d ?dTo')
+        goal.modify(d=m, dTo=y, check='False', tree='None')
+        if debug:
+            print(f'BLOCKER: {goal.chunk}    DEPTH: {depth}\n\n\tADD DMBuffer: {p} {depth} {m} {y} {d} {dTo}\n')
+
+    def move(goal='p:?p pTo:?pTo d:!None?d dTo:!None?dTo check:True?check recall:?recall', towers='m:None?m y:!None?y', imaginal='?depth'):
         towers.move(d, dTo)
         towers.satisfy(p, pTo)
-        DM.request('?p ?d ?dTo ? ?')
+        DM.request('?p ?depth ?d ?dTo ? ?')
         goal.modify(d='None', dTo='None', check='False', recall='True')
+        if debug:
+            print(f'\nMOVE: {goal.chunk}    DEPTH: {depth}\n\n\tREQ DMBuffer: {p} {depth} {d} {dTo} ? ?\n')
 
-    def recall(goal='recall:True?recall', DMBuffer='? ? ? ?d ?dTo', towers='s:False'):
-        goal.modify(d=d, dTo=dTo, recall='False')
+    def recall(goal='recall:True?recall tree:None', DMBuffer='? ? ? ? ?d ?dTo', towers='s:False', imaginal='?depth'):
+        DM.request('count ? ?depth')
+        goal.modify(d=d, dTo=dTo, recall='False', tree='Up')
+        if debug:
+            print(f'RECALL: {goal.chunk}    DEPTH: {depth}\n\n\tREC DMBuffer: {DMBuffer.chunk}\n')
 
-    def satisfy(goal='p:?p d:?d dTo:?dTo recall:True?recall count:?count', towers='s:True?s'):
-        DM.request('count ? ?p')
-        goal.modify(count='True')
-
-    def count(goal='count:True?count', DMBuffer='count ?n ?'):
+    def up(goal='tree:Up?tree', DMBuffer='count ?n ?', imaginal='?depth'):
         imaginal.set('?n')
+        goal.modify(tree='None')
+        if debug:
+            print(f'UP: {goal.chunk}    DEPTH: {depth}')
 
-    def change(goal='p:?p count:True?count', imaginal='!None?n'):
+    def satisfy(goal='p:?p d:?d dTo:?dTo recall:True?recall satisfied:False?satisfied', towers='s:True?s', imaginal='?depth'):
+        DM.request('count ? ?p')
+        goal.modify(satisfied='True')
+        if debug:
+            print(f'SATISFY: {goal.chunk}    DEPTH: {depth}')
+
+    def change(goal='p:?p satisfied:True?satisfied', DMBuffer='count ?n ?', imaginal='?depth'):
         imaginal.set('None')
-        goal.modify(p=n, d='None', dTo='None', count='False', recall='False')
+        goal.modify(p=n, d='None', dTo='None', recall='False', satisfied='False')
+        if debug:
+            print(f'CHANGE: {goal.chunk}    DEPTH: {depth}')
 
 
 if __name__ == "__main__":
     env = WrapperEnv()
-    env.agent = MyAgent()
+    env.agent = AlgorithmicAgent()
     env.agent.towers = env.towers
     env.run()
     print()
